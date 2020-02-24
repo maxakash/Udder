c3149ede222eabc7755ac3015b69709396810b51
maxakash <akashchaudharymax@gmail.com>
2020-02-24 23:06:39 +0530

    added wildidappear

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    override func viewWillDisappear(_ animated: Bool) {
+        print("akash")
+    }
 
 
 }
